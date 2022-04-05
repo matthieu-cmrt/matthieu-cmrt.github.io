fe177ba9059e743d61de9ee18ac684c3e302cc53
matthieu-cmrt <matthieu.camart@gmail.com>
2022-04-05 10:53:53 +0100

    Javascript: Adding the calculator

---
+++ portfolio/js/calculator-modules/perform-calculations.js
@@ -0,0 +1,15 @@
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    } else if (operator === '-') {
+        return firstOperand - secondOperand;
+    } else if (operator === '*') {
+        return firstOperand * secondOperand;
+    } else if (operator === '/') {
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+}
+    
+export function calculate(firstOperand, secondOperand, operator);
